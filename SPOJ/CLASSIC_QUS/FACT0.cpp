f7777b06abce49a891972d488c887fb2742c1c84
Manish Kumar <manu1.uec2014@gmail.com>
2017-10-19 04:21:36 +0530

    Create FACT0.cpp

---
+++ SPOJ/CLASSIC_QUS/FACT0.cpp
@@ -0,0 +1,109 @@
+//#include<bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <climits>
+#include <cstring>
+#include <vector>
+#include <map>
+using namespace std;
+#define pi 3.1415926535897
+#define ll long long
+#define si(n) scanf("%d",&n)
+#define si2(n1,n2) scanf("%d%d",&n1,&n2)
+#define si3(n1,n2,n3) scanf("%d%d%d",&n1,&n2,&n3)
+#define sll(n) scanf("%lld",&n)
+#define sll2(n1,n2) scanf("%lld%lld",&n1,&n2)
+#define sll3(n1,n2,n3) scanf("%lld%lld%lld",&n1,&n2,&n3)
+#define pin(n) printf("%d\n",n)
+#define mem(A,i) memset(A, i, sizeof(A))
+#define plln(n) printf("%lld\n",n)
+#define pis(n) printf("%d ",n)
+#define plls(n) printf("%lld ",n)
+#define rep(i, start, end) for(int i=start; i<end; i++)
+#define repDown(i, start, end) for(ll i=start; i>=end; i--)
+#define P pair<ll,ll>
+#define PP pair<P,ll>
+#define mod 1000000007
+#define MAX 100005
+#define s second
+#define f first
+#define newLine printf("\n")
+#define pb push_back
+#define sieveMax 40000001
+bool neverMakeThisName[sieveMax];
+vector<int> Prime;
+
+
+void sieve(){
+    neverMakeThisName[0]=neverMakeThisName[1]=true;
+    for(int i=4; i<sieveMax; i+=2) neverMakeThisName[i]=true;
+
+    for(int i=3; i<=sqrt(sieveMax); i+=2)
+        if(neverMakeThisName[i]==false) {
+            for(int j=i*i; j<sieveMax; j+=i) neverMakeThisName[j]=true;
+        }
+
+    rep(i,0,sieveMax) if(neverMakeThisName[i]==false) Prime.pb(i);
+}
+
+ll gcd ( ll  a, ll b ){
+  if ( a==0 ) return b;
+  return gcd ( b%a, a );
+}
+string inputString(){
+    string i="";
+    int temp=getchar();
+    while(temp<'a'||temp>'z')
+        temp=getchar();
+    while(temp>='a'&&temp<='z')
+    {
+        i+=(char)temp;
+        temp=getchar();
+    }
+    return i;
+}
+
+ll expo(ll x, ll n, ll m){
+	if(n==0) return 1;
+	ll temp=expo(x,n/2,m);
+	temp = ((temp%m)*(temp%m))%m;
+	if(n%2!=0) temp = ((temp%m)*(x%m))%m;
+	return temp;
+}
+
+long long add(long long &x, long long y){
+        x=x+y;
+        x%=mod;
+        if(x<0) x+=mod;
+        return x;
+}
+ll n;
+void solve(){
+    //solve the problem. You can and you will :) give your best shot..
+    ll roo=sqrt(n);
+    bool first=true;
+    rep(i,0,Prime.size()){
+        if(Prime[i]>roo) break;
+        int cnt=0;
+        while(n>1 && (n%Prime[i])==0){
+            cnt++;
+            n/=Prime[i];
+        }
+        if(cnt) {
+            printf("%d^%d ", Prime[i], cnt);
+        }
+    }
+    if(n>1) {
+        printf("%lld^%d", n, 1);
+    }
+    newLine;
+}
+
+int main(){
+    sieve();
+    while(sll(n)){
+        if(!n) break;
+        solve();
+    }
+    return 0;
+}
