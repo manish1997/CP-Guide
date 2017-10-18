c1e8d7e4b56b9eec8d06616691c918e8f9bf9f0b
Manish Kumar <manu1.uec2014@gmail.com>
2017-10-19 04:10:37 +0530

    added clone

---
+++ SPOJ/CLASSIC_QUS/CLONE.cpp
@@ -0,0 +1,106 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <climits>
+#include <cstring>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+#define pi 3.1415926535897
+#define ll long long
+#define si(n) scanf("%d",&n)
+#define si2(n1,n2) scanf("%d%d",&n1,&n2)
+#define si3(n1,n2,n3) scanf("%d%d%d",&n1,&n2,&n3)
+#define sll(n) scanf("%lld",&n)
+#define sll2(n1,n2) scanf("%lld%lld",&n1,&n2)
+#define sll3(n1,n2,n3) scanf("%lld%lld%lld",&n1,&n2,&n3)
+#define pin(n) printf("%d\n",n)
+#define mem(A,i) memset(A, i, sizeof(A))
+#define plln(n) printf("%lld\n",n)
+#define pis(n) printf("%d ",n)
+#define plls(n) printf("%lld ",n)
+#define rep(i, start, end) for(int i=start; i<end; i++)
+#define repDown(i, start, end) for(int i=start; i>=end; i--)
+#define P pair<int,int>
+#define PP pair<P,int>
+#define mod 1000000007
+#define MAX 100005
+#define s second
+#define f first
+#define newLine printf("\n")
+#define pb push_back
+#define sieveMax 10001 //maximum for which u need primality test
+
+//Sieve Start
+vector<int> Prime;
+void sieve(){
+    bool neverMakeThisName[sieveMax];
+    mem(neverMakeThisName,false);
+    neverMakeThisName[0]=neverMakeThisName[1]=true;
+    for(int i=4; i<sieveMax; i+=2) neverMakeThisName[i]=true;
+
+    for(int i=3; i<=sqrt(sieveMax); i+=2)
+        if(neverMakeThisName[i]==false) {
+            for(int j=i*i; j<sieveMax; j+=i) neverMakeThisName[j]=true;
+        }
+
+    rep(i,0,sieveMax) if(neverMakeThisName[i]==false) Prime.pb(i);
+}
+//Sieve End
+ll gcd ( ll  a, ll b ){
+  if ( a==0 ) return b;
+  return gcd ( b%a, a );
+}
+string inputString(){
+    string i="";
+    int temp=getchar();
+    while(temp<'A'||temp>'Z')
+        temp=getchar();
+    while(temp>='A'&&temp<='Z')
+    {
+        i+=(char)temp;
+        temp=getchar();
+    }
+    return i;
+}
+
+ll expo(ll x, ll n, ll m){
+    if(n==0) return 1;
+    ll temp=expo(x,n/2,m);
+    temp = ((temp%m)*(temp%m))%m;
+    if(n%2!=0) temp = ((temp%m)*(x%m))%m;
+    return temp;
+}
+
+long long add(long long &x, long long y){
+        x=x+y;
+        x%=mod;
+        if(x<0) x+=mod;
+        return x;
+}
+int n,m; 
+unordered_map<string,int> mapp;
+void solve(){
+    //solve the problem. You can and you will :) give your best shot..
+    mapp.clear();
+    rep(i,0,n){
+        string s=inputString();
+        mapp[s]++;
+    }
+    int ans[20002];
+    mem(ans,0);
+    for(auto it=mapp.begin(); it!=mapp.end(); it++){
+        ans[it->second]++;
+    }
+    rep(i,1,n+1) pin(ans[i]);
+    
+}
+
+int main(){
+    while(true){
+        si2(n,m);
+        if(n==0 && m==0) break;
+        solve();
+    }
+    return 0;
+}
